ad3f4a3dde79f643079559c8389822e1a7bb1de2
sakichat <saki28chat@gmail.com>
2017-02-24 16:37:58 -0500

    backpackpanel
    backpackpanel

---
+++ src/game/BackpackPanel.java
@@ -0,0 +1,55 @@
+package game;
+
+import ui.EquipmentView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * Created by Saki on 2017/2/24.
+ */
+public class BackpackPanel extends JPanel{
+
+    public BackpackPanel(){
+        this.setLayout(null);
+        this.setSize(460, 330);
+
+        initSubviews();
+    }
+
+    private void initSubviews(){
+        Label label = new Label("Backpack");
+        label.setSize(460, 20);
+        label.setLocation(0, 0);
+        this.add(label);
+
+        int x = 10;
+        int y = 30;
+
+        Player player = new Player();
+        List<Equipment> backpack;
+        backpack = player.equipmentsInBackpack();
+
+        for (Equipment equipment : backpack) {
+            EquipmentView equipmentView = new EquipmentView();
+            equipmentView.setLocation(x, y);
+            this.add(equipmentView);
+            equipmentView.setEquipment(equipment);
+
+            Button equipButton = new Button();
+            Button dropButtton = new Button();
+
+            equipButton.setSize(60, 20);
+            equipButton.setLocation(310, y);
+            this.add(equipButton);
+
+            dropButtton.setSize(60, 20);
+            dropButtton.setLocation(380, y);
+            this.add(dropButtton);
+
+            y += 30;
+        }
+    }
+}
